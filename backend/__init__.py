f55ec126196207c1d9ffeeb2082d4d2f1f8e0c5c
Allen Dai <allendai@princeton.edu>
2020-04-06 18:46:22 -0500

    updated the path

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__,
-            static_url_path='',
+            static_url_path='TigerPhotos',
             static_folder='frontend/build')
 app.config.from_object(Config)
 
