3c9e3426b9e529b5e54b1378bdb57206f400086d
Allen Dai <allendai@princeton.edu>
2020-04-09 14:19:21 -0500

    heroku test

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ea8228e88742231dc7c3635b5ab8f4d.js"
+  "/precache-manifest.123674ae0d2ca15aebddc828e5ff3d31.js"
 );
 
 self.addEventListener('message', (event) => {
