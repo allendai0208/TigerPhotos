18911484ef29b815111f7c70a7bfc33fb96c9e29
keithregister <kregister26@gmail.com>
2020-04-11 18:25:41 -0400

    abous us update

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -14,12 +14,13 @@ class AboutUs extends React.Component {
     render() {   
         return (
             <div>
-                <p className = "aboutHeader">About Tiger Photos</p>
+                <br/>
+                <p className = "aboutHeader">About TigerPhotos</p>
                 <p className = 'aboutParagraph'> 
-                    Tiger Photos is a centralized location to hire student photographers, videographers, and video editors for student jobs around campus.
+                    TigerPhotos is a centralized location to hire student photographers, videographers, and video editors for student jobs around campus.
                 </p>
                 <p className = 'aboutParagraph'>
-                    Tiger Photos was originally built by a group of students as their final project for COS 333: Advanced Programming Techniques taught by Robert Dondero in Spring 2020.
+                    TigerPhotos was originally built by a group of students as their final project for COS 333: Advanced Programming Techniques taught by Robert Dondero in Spring 2020.
                 </p>
 
                 <p className = 'aboutHeader'>Original Team</p>
@@ -30,31 +31,31 @@ class AboutUs extends React.Component {
                                 <img className = "aboutImage" src = {keith}></img>
                                 <p className = "aboutName">Keith Register</p>
                                 <p className = "aboutJob">Front end</p>
-                                <p className = "aboutParagraph">Hi! I'm from New Jersey and I like to play soccer, basketball, and volleyball in my free time.</p>
+                                <p className = "aboutParagraph">Hi! I'm from New Jersey and I like to play soccer, basketball, and volleyball in my free time. I am also better than Aaron at Smash Bros.</p>
                             </Col>
                             <Col>
                                 <img className = "aboutImage" src = {aaron}></img>
                                 <p className = "aboutName">Aaron Nguyen</p>
                                 <p className = "aboutJob">Back end</p>
-                                <p className = "aboutParagraph">.</p>
-                            </Col>
-                            <Col>
-                                <img className = "aboutImage" src = {alicia}></img>
-                                <p className = "aboutName">Alicia Liu</p>
-                                <p className = "aboutJob">Front end</p>
-                                <p className = "aboutParagraph">.</p>
+                                <p className = "aboutParagraph">Hey! I'm from Sacramento, CA. I like to go to the gym and play video games such as League of Legends and Super Smash Bros in my free time.</p>
                             </Col>
                             <Col>
                                 <img className = "aboutImage" src = {allen}></img>
                                 <p className = "aboutName">Allen Dai</p>
                                 <p className = "aboutJob">Supreme Leader</p>
-                                <p className = "aboutParagraph"></p>
+                                <p className = "aboutParagraph">I am from Chicago, and I spend my free time bossing my subordinates around.</p>
+                            </Col>
+                            <Col>
+                                <img className = "aboutImage" src = {alicia}></img>
+                                <p className = "aboutName">Alicia Liu</p>
+                                <p className = "aboutJob">Front end</p>
+                                <p className = "aboutParagraph">I'm from Seattle, Washington and I like to hang out with Allen in my free time.</p>
                             </Col>
                             <Col>
                                 <img className = "aboutImage" src = {mwad}></img>
                                 <p className = "aboutName">Mwad Saleh</p>
                                 <p className = "aboutJob">Back end</p>
-                                <p className = "aboutParagraph"></p>
+                                <p className = "aboutParagraph">I am from Libya and I used to play soccer in my free time before my knee blew out.</p>
                             </Col>
                         </Row>
                     </Container>
