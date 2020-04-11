fec0dc0017c09571949c7d7d89655d5a9a0bf26b
keithregister <kregister26@gmail.com>
2020-04-11 17:39:15 -0400

    about us page changes

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -1,50 +1,66 @@
 import React from "react"
-import Grid from '@material-ui/core/Grid'
 import keith from './pictures/keithPicture.JPG'
 import alicia from './pictures/aliciaPicture.jpg'
 import aaron from './pictures/aaronPicture.jpg'
 import allen from './pictures/allenPicture.jpg'
-import mwad from './pictures/mwadPicture.jpeg'
+import mwad from './pictures/mwadPicture.jpg'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import logo from "./pictures/tigerLogo.png"
 
 class AboutUs extends React.Component {
 
     render() {   
         return (
             <div>
-                <h1 className = "aboutHeader">About Tiger Photos</h1>
+                <p className = "aboutHeader">About Tiger Photos</p>
                 <p className = 'aboutParagraph'> 
-                    Tiger Photos is a centralized location to hire student photogrpahers, videographers, and video editors for student jobs around campus.
+                    Tiger Photos is a centralized location to hire student photographers, videographers, and video editors for student jobs around campus.
                 </p>
                 <p className = 'aboutParagraph'>
                     Tiger Photos was originally built by a group of students as their final project for COS 333: Advanced Programming Techniques taught by Robert Dondero in Spring 2020.
                 </p>
 
-                <h1 className = 'aboutHeader'>Original Team</h1>
-                <div className = 'aboutGridContainer'>
-                    <Grid className = 'aboutGrid' container spacing={3}>
-                        <Grid item xs>
-                            <img src = {keith} className = "aboutImage"></img>
-                        </Grid>
-                        <Grid item sm>
-                            <img src = {alicia} className = "aboutImage"></img>
-                        </Grid>
-                        <Grid item md>
-                            <img src = {aaron} className = "aboutImage"></img>
-                        </Grid>
-                        <Grid item lg>
-                            <img src = {allen} className = "aboutImage"></img>
-                        </Grid>
-                        <Grid item xl>
-                            <img src = {mwad} className = "aboutImage"></img>
-                        </Grid>
-                    
-                    </Grid>
+                <p className = 'aboutHeader'>Original Team</p>
+                <div  className = "aboutGrid">
+                    <Container fluid>
+                        <Row>
+                            <Col>
+                                <img className = "aboutImage" src = {keith}></img>
+                                <p className = "aboutName">Keith Register</p>
+                                <p className = "aboutJob">Front end</p>
+                                <p className = "aboutParagraph">Hi! I'm from New Jersey and I like to play soccer, basketball, and volleyball in my free time.</p>
+                            </Col>
+                            <Col>
+                                <img className = "aboutImage" src = {aaron}></img>
+                                <p className = "aboutName">Aaron Nguyen</p>
+                                <p className = "aboutJob">Back end</p>
+                                <p className = "aboutParagraph">.</p>
+                            </Col>
+                            <Col>
+                                <img className = "aboutImage" src = {alicia}></img>
+                                <p className = "aboutName">Alicia Liu</p>
+                                <p className = "aboutJob">Front end</p>
+                                <p className = "aboutParagraph">.</p>
+                            </Col>
+                            <Col>
+                                <img className = "aboutImage" src = {allen}></img>
+                                <p className = "aboutName">Allen Dai</p>
+                                <p className = "aboutJob">Supreme Leader</p>
+                                <p className = "aboutParagraph"></p>
+                            </Col>
+                            <Col>
+                                <img className = "aboutImage" src = {mwad}></img>
+                                <p className = "aboutName">Mwad Saleh</p>
+                                <p className = "aboutJob">Back end</p>
+                                <p className = "aboutParagraph"></p>
+                            </Col>
+                        </Row>
+                    </Container>
                 </div>
+                <img className = "tigerLogo" src = {logo}></img>
             </div>
-
-
-
-
         )
     }
 }
