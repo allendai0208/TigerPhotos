dcc3197b68754d539ffd26faaf3d32df43f02001
Allen Dai <allendai@princeton.edu>
2020-04-17 13:41:46 -0500

    adding build files

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.17a600d26c78dc704c9417e4ebda8328.js"
+  "/precache-manifest.ac96c66d68a020034bb8158cdcf84525.js"
 );
 
 self.addEventListener('message', (event) => {
